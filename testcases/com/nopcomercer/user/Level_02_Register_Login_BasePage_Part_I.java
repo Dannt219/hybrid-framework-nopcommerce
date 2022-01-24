5d4379c6c1e2fa5d0b60fe422dd0937f06bdc9d1
Admin <Admin@NTD2109>
2022-01-24 21:56:27 +0700

    Continue Testcase register

--- testcases/com/nopcomercer/user/Level_02_Register_Login_BasePage_Part_I.java
+++ testcases/com/nopcomercer/user/Level_02_Register_Login_BasePage_Part_I.java
@@ -1,11 +1,13 @@
 package com.nopcomercer.user;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,31 +18,143 @@ public class Level_02_Register_Login_BasePage_Part_I {
 	WebDriver driver;
 	String emailAddress;
 	String projectPath = System.getProperty("user.dir");
-	
+
 	Select select;
 	WebDriverWait explicitWait;
 	BasePage basePage;
 
-  @BeforeClass
-  public void beforeClass() {
-	  System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-	  driver = new FirefoxDriver();
-	  
-	  basePage = new BasePage();
-	  emailAddress = "";
-	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-  }
-  
-  @Test
-  public void Register_Empty_Data() {
-	  basePage.openUrl(driver, "https://demo.nopcommerce.com/");
-	  
-	  basePage.clickToElement(driver, "");
-	  basePage.clickToElement(driver, "");
-  }
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
 
-  @AfterClass
-  public void afterClass() {
-  }
+		basePage = new BasePage();
+		emailAddress = "ntd" + randomEmail() + "@email.vn";
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+	}
+
+	@Test
+	public void TC_01_Register_Empty_Data() {
+		//basePage.openUrl(driver, "https://demo.nopcommerce.com/");
+
+		basePage.waitForElementClickable(driver, "//a[@class='ico-register']");
+		basePage.clickToElement(driver, "//a[@class='ico-register']");
+
+		basePage.waitForElementClickable(driver, "//button[@id='register-button']");
+		basePage.clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='FirstName-error']"),
+				"First name is required.");
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='LastName-error']"), "Last name is required.");
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='Email-error']"), "Email is required.");
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='Password-error']"), "Password is required.");
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='ConfirmPassword-error']"),
+				"Password is required.");
+	}
+
+	@Test
+	public void TC_02_Register_Ivalid_Email() {
+		basePage.waitForElementClickable(driver, "//a[@class='ico-register']");
+		basePage.clickToElement(driver, "//a[@class='ico-register']");
+
+		basePage.senkeyToElement(driver, "//input[@id='FirstName']", "Aidan");
+		basePage.senkeyToElement(driver, "//input[@id='LastName']", "Nguyen");
+		basePage.senkeyToElement(driver, "//input[@id='Email']", "123");
+		basePage.senkeyToElement(driver, "//input[@id='Password']", "123456");
+		basePage.senkeyToElement(driver, "//input[@id='ConfirmPassword']", "123456");
+
+		basePage.waitForElementClickable(driver, "//button[@id='register-button']");
+		basePage.clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='Email-error']"), "Wrong email");
+
+	}
+
+	@Test
+	public void TC_03_Register_Success() {
+		basePage.waitForElementClickable(driver, "//a[@class='ico-register']");
+		basePage.clickToElement(driver, "//a[@class='ico-register']");
+
+		basePage.senkeyToElement(driver, "//input[@id='FirstName']", "Aidan");
+		basePage.senkeyToElement(driver, "//input[@id='LastName']", "Nguyen");
+		basePage.senkeyToElement(driver, "//input[@id='Email']", emailAddress);
+		basePage.senkeyToElement(driver, "//input[@id='Password']", "123456");
+		basePage.senkeyToElement(driver, "//input[@id='ConfirmPassword']", "123456");
+
+		basePage.waitForElementClickable(driver, "//button[@id='register-button']");
+		basePage.clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(basePage.getElementText(driver, "//div[@class='result']"), "Your registration completed");
+
+		basePage.waitForElementClickable(driver, "//a[@class='ico-logout']");
+		basePage.clickToElement(driver, "//a[@class='ico-logout']");
+	}
+
+	@Test
+	public void TC_04_Register_Existing_Email() {
+		basePage.waitForElementClickable(driver, "//a[@class='ico-register']");
+		basePage.clickToElement(driver, "//a[@class='ico-register']");
+
+		basePage.senkeyToElement(driver, "//input[@id='FirstName']", "Aidan");
+		basePage.senkeyToElement(driver, "//input[@id='LastName']", "Nguyen");
+		basePage.senkeyToElement(driver, "//input[@id='Email']", "ntd219@mail.vn");
+		basePage.senkeyToElement(driver, "//input[@id='Password']", "123456");
+		basePage.senkeyToElement(driver, "//input[@id='ConfirmPassword']", "123456");
+
+		basePage.waitForElementClickable(driver, "//button[@id='register-button']");
+		basePage.clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(basePage.getElementText(driver, "//div[contains(@class,'message-error')]//li"),
+				"The specified email already exists");
+
+	}
+
+	@Test
+	public void TC_05_Register_Password_Less() {
+		basePage.waitForElementClickable(driver, "//a[@class='ico-register']");
+		basePage.clickToElement(driver, "//a[@class='ico-register']");
+
+		basePage.senkeyToElement(driver, "//input[@id='FirstName']", "Aidan");
+		basePage.senkeyToElement(driver, "//input[@id='LastName']", "Nguyen");
+		basePage.senkeyToElement(driver, "//input[@id='Email']", emailAddress);
+		basePage.senkeyToElement(driver, "//input[@id='Password']", "123");
+		basePage.senkeyToElement(driver, "//input[@id='ConfirmPassword']", "123");
+
+		basePage.waitForElementClickable(driver, "//button[@id='register-button']");
+		basePage.clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='Password-error']"),
+				"Password must meet the following rules:\nmust have at least 6 characters");
+	}
+
+	@Test
+	public void TC_06_Register_Invalid_Confirm_Password() {
+		basePage.waitForElementClickable(driver, "//a[@class='ico-register']");
+		basePage.clickToElement(driver, "//a[@class='ico-register']");
+
+		basePage.senkeyToElement(driver, "//input[@id='FirstName']", "Aidan");
+		basePage.senkeyToElement(driver, "//input[@id='LastName']", "Nguyen");
+		basePage.senkeyToElement(driver, "//input[@id='Email']", emailAddress);
+		basePage.senkeyToElement(driver, "//input[@id='Password']", "123456");
+		basePage.senkeyToElement(driver, "//input[@id='ConfirmPassword']", "123457");
+
+		basePage.waitForElementClickable(driver, "//button[@id='register-button']");
+		basePage.clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(basePage.getElementText(driver, "//span[@id='ConfirmPassword-error']"),
+				"The password and confirmation password do not match.");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int randomEmail() {
+		Random rand = new Random();
+		return rand.nextInt(1000);
+
+	}
 
 }
